7f648db1521a2f0658e2ed03c0af0f075fb6ef38
dx9 <jianglxu@gmail.com>
2020-11-18 00:00:18 +0800

    修改函数顺序

--- BeautifulString.c
+++ BeautifulString.c
@@ -1,84 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-    char input[] = "input.txt";
-    char output[] = "output.txt";
-    FILE *inputFp;
-    FILE *outputFp;
-    if ((inputFp = fopen(input, "r")) == NULL)
-    {
-        printf("open input file error!");
-        return -1;
-    }
-    if ((outputFp = fopen(output, "w")) == NULL)
-    {
-        fclose(inputFp);
-        printf("open output file error!");
-        return -1;
-    }
-
-    const int inputLength = 128;
-    const int outputLength = 1024;
-    char inputLine[inputLength];
-    char beautifulString[inputLength];
-    char outputLine[outputLength];
-    memset(outputLine, 0, outputLength);
-    int lineNum = 1;
-    while (!feof(inputFp))
-    {
-        memset(inputLine, 0, inputLength);
-        fgets(inputLine, inputLength, inputFp);
-        int length = strlen(inputLine);
-        char finded = 0;
-        if (length > 0)
-        {
-            int begin = 0;
-            while (begin <= length - 6)
-            {
-                if (aabbcc(inputLine, begin, length, beautifulString))
-                    save(outputLine, beautifulString, lineNum, &finded);
-                begin += strlen(beautifulString);
-            }
-
-            begin = 0;
-            while (begin <= length - 3)
-            {
-                if (abc(inputLine, begin, length, beautifulString))
-                    save(outputLine, beautifulString, lineNum, &finded);
-                begin += strlen(beautifulString);
-            }
-
-            begin = 0;
-            while (begin <= length - 3)
-            {
-                if (bbb(inputLine, begin, length, beautifulString))
-                    save(outputLine, beautifulString, lineNum, &finded);
-                begin += strlen(beautifulString);
-            }
-
-            begin = 0;
-            while (begin <= length - 6)
-            {
-                if (acacac(inputLine, begin, length, beautifulString))
-                    save(outputLine, beautifulString, lineNum, &finded);
-                begin += strlen(beautifulString);
-            }
-        }
-        if (finded)
-        {
-            strcat(outputLine, "\n");
-            fputs(outputLine, outputFp);
-            memset(outputLine, 0, outputLength);
-        }
-        lineNum++;
-    }
-
-    fclose(inputFp);
-    fclose(outputFp);
-    return 0;
-}
 
 int abc(char *input, int begin, int length, char *beautifulString)
 {
@@ -215,4 +137,84 @@ void save(char *outputLine, char *beautifulString, int lineNum, char *finded)
         *finded = 1;
     }
     strcat(outputLine, beautifulString);
+}
+
+
+int main()
+{
+    char input[] = "input.txt";
+    char output[] = "output.txt";
+    FILE *inputFp;
+    FILE *outputFp;
+    if ((inputFp = fopen(input, "r")) == NULL)
+    {
+        printf("open input file error!");
+        return -1;
+    }
+    if ((outputFp = fopen(output, "w")) == NULL)
+    {
+        fclose(inputFp);
+        printf("open output file error!");
+        return -1;
+    }
+
+    const int inputLength = 128;
+    const int outputLength = 1024;
+    char inputLine[inputLength];
+    char beautifulString[inputLength];
+    char outputLine[outputLength];
+    memset(outputLine, 0, outputLength);
+    int lineNum = 1;
+    while (!feof(inputFp))
+    {
+        memset(inputLine, 0, inputLength);
+        fgets(inputLine, inputLength, inputFp);
+        int length = strlen(inputLine);
+        char finded = 0;
+        if (length > 0)
+        {
+            int begin = 0;
+            while (begin <= length - 6)
+            {
+                if (aabbcc(inputLine, begin, length, beautifulString))
+                    save(outputLine, beautifulString, lineNum, &finded);
+                begin += strlen(beautifulString);
+            }
+
+            begin = 0;
+            while (begin <= length - 3)
+            {
+                if (abc(inputLine, begin, length, beautifulString))
+                    save(outputLine, beautifulString, lineNum, &finded);
+                begin += strlen(beautifulString);
+            }
+
+            begin = 0;
+            while (begin <= length - 3)
+            {
+                if (bbb(inputLine, begin, length, beautifulString))
+                    save(outputLine, beautifulString, lineNum, &finded);
+                begin += strlen(beautifulString);
+            }
+
+            begin = 0;
+            while (begin <= length - 6)
+            {
+                if (acacac(inputLine, begin, length, beautifulString))
+                    save(outputLine, beautifulString, lineNum, &finded);
+                begin += strlen(beautifulString);
+            }
+        }
+        if (finded)
+        {
+            strcat(outputLine, "\n");
+            fputs(outputLine, outputFp);
+            memset(outputLine, 0, outputLength);
+        }
+        lineNum++;
+    }
+
+    fclose(inputFp);
+    fclose(outputFp);
+    return 0;
 }
